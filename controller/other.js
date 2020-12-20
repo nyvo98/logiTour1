71377e56c144fd0150b62017eabc0230ff34b9d9
Ubuntu <ubuntu@ip-172-31-40-70.ap-southeast-1.compute.internal>
2020-12-20 05:18:53 +0000

    update

--- controller/other.js
+++ controller/other.js
@@ -19,8 +19,8 @@ export default class OtherServices {
           payment_method: 'paypal'
         },
         redirect_urls: {
-          return_url: 'http://localhost:30332/api/tour/paypalResponse',
-          cancel_url: 'http://localhost:30332/api/tour/paypalResponse'
+          return_url: 'http://localhost:3032/api/tour/paypalResponse',
+          cancel_url: 'http://localhost:3032/api/tour/paypalResponse'
         },
         transactions: [{
           item_list: {
